a5dfe6b08d3468097158b43fb6f3684236a9e848
Nicolas GORAJ <github@goraj.fr>
2025-03-23 10:16:38 +0100

    feat: activate chordal hold

--- keyboards/beekeeb/3w6hs/config.h
+++ keyboards/beekeeb/3w6hs/config.h
@@ -20,3 +20,6 @@
 #define I2C_DRIVER I2CD0
 #define I2C1_SDA_PIN GP0
 #define I2C1_SCL_PIN GP1
+
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
--- keyboards/beekeeb/3w6hs/keymaps/sun2140/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/sun2140/keymap.c
@@ -65,6 +65,13 @@ enum layers {
     _CFG
 };
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_split_3x5_3(
+        'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
+                  'L', 'L', 'L',  'R', 'R', 'R'
+    );
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ALPHA] = LAYOUT_split_3x5_3(
